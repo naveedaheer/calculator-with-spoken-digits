5b3219df9ba22d0b6f84df35e87f571f4d6128c2
Naveed Aheer <naveedaheer@gmail.com>
2019-11-23 19:29:49 +0500

    delete unused component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
-import { AgeComponent } from './age/age.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
-    AgeComponent
   ],
   imports: [
     BrowserModule,
